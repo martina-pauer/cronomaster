552d47aabeb12c2a81e6031c8b1cd6610cc366ac
Martina Pauer <martinapauer38@gmail.com>
2025-08-01 12:58:31 +0000

    Base module: the basic for the time representacion

---
+++ chrono.py
@@ -0,0 +1,31 @@
+import time
+
+class TimeWallet():
+    def __init__(self, secs: int):
+        '''
+            Represent a seconds storage
+            and data sender
+        '''
+        self.seconds: int = secs
+        self.ID = hash(self)
+
+    def send(self, secs: int, obj):
+        '''
+            Send some seconds to other TimeWallet
+            object when is possible
+        '''
+        if self.seconds >= secs:
+            obj.seconds += secs
+        else:
+            while self.seconds <= secs:
+                if self.ID != obj.ID:
+                    self.count()            
+            self.send(secs, obj)
+
+    def count(self):
+        '''
+            Count one second more
+            using the standard
+            module time
+        '''            
+        self.seconds += time.gmtime().tm_sec            
