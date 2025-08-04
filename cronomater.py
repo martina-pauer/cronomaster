98c372c4d3d7cd2aa4a5e5132d6b626799fa2a5d
Martina Pauer <martinapauer38@gmail.com>
2025-08-04 12:34:49 +0000

    Restart data: conromaster.py <seconds>
    Now the files contains zero seconds to be more
    coherent wiht the reality of initil state
    and take as paremeter the seconds to send

--- cronomater.py
+++ cronomater.py
@@ -1,12 +1,13 @@
 import os
+import sys
 from chrono import TimeWallet
 # Get storaged seconds from external source later
 def read_wallet(name: str) -> int:
     with open(name, 'rb') as wallet:
         result = wallet.read().hex()
     return int(result, 16)
-origin = TimeWallet(read_wallet('first.dat'))
-destination = TimeWallet(read_wallet('second.dat'))
+origin = TimeWallet(0)#read_wallet('first.dat'))
+destination = TimeWallet(0)#read_wallet('second.dat'))
 # Show initial state
 def save_wallet(first: TimeWallet, name: str):
     with open(name, 'wb') as one:
@@ -24,11 +25,11 @@ show_wallets(origin, destination)
 option = 'y'
 while option.lower() == 'y':
     # Send seconds and count more seconds only when has underrate
-    origin.send(7, destination)
-    # Next state after send 7s to destination
+    origin.send(int(sys.argv[1]), destination)
+    # Next state after send seconds to destination
     show_wallets(origin, destination)
-    save_wallet(origin, 'first.dat')
-    save_wallet(destination, 'second.dat')
+    #save_wallet(origin, 'first.dat')
+    #save_wallet(destination, 'second.dat')
     option = input('\nContinue Y/n: ')
 # Clean cacche
 os.system('rm -R __pycache__')    
