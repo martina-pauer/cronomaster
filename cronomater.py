1e4ec9ada127bd7a9a17d06fb239dd09737dd6e9
Martina Pauer <martinapauer38@gmail.com>
2025-08-04 13:54:16 +0000

    More practical script: take out parameters
    Make more customizable the seconds to send
    and hash id for making more flexible
    the time distribution aporting value and control

--- cronomater.py
+++ cronomater.py
@@ -17,22 +17,22 @@ def show_wallets(start: TimeWallet, end: TimeWallet):
     sep = ''
     for repeat in range(0, 29):
         sep += '-'
-    print(f'\t{sep}\n\t{start.ID} wallet has {start.seconds}s\n\t{end.ID} wallet has {end.seconds}s')
+    print(f'\t{sep}\n\t{start.ID} [Your] wallet has {start.seconds}s\n\t[Waiting] wallet has {end.seconds}s')
 # First state
-print('\t[CRONOMASTER]\n\nSend seconds between conected wallets')
+print('\n\t[CRONOMASTER]\n\nSend seconds between conected wallets')
 show_wallets(origin, destination)    
 # Make the right changes
 option = 'y'
 while option.lower() == 'y':
     # Send seconds and count more seconds only when has underrate
-    origin.send(int(sys.argv[1]), destination)
+    origin.send(int(input(f'\n\tWrite seconds to send from {origin.ID}: ')), destination)
     # Next state after send seconds to destination
     show_wallets(origin, destination)
     #save_wallet(origin, 'first.dat')
     #save_wallet(destination, 'second.dat')
     option = input('\nContinue Y/n: ')
 # Receive from the other active script the new data for origin object
-bridge = sys.argv[2].__str__()
+bridge = input('\nWrite hash id for receptor wallet: ').__str__()
 #bridge.receive()
 # Send later to the script with ID from destination to origin object
 #bridge.send(destination, bridge.source.origin)
