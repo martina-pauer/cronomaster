489bf01bec6fd8f1270cd8f212b7ed74d52f4a85
Martina Pauer <martinapauer38@gmail.com>
2025-08-05 13:00:19 +0000

    Fix Code: more extensible
    Add new output for show_wallet getting more use
    for future programs and communication function
    take Loop Back IP and free listening port making
    useful the script pointing from one device with
    the IP address of the other device

--- cronomater.py
+++ cronomater.py
@@ -15,23 +15,33 @@ def read_wallet(name: str) -> int:
 origin = TimeWallet(0)#read_wallet('first.dat'))
 destination = TimeWallet(0)#read_wallet('second.dat'))
 
-def communication():   
+def communication(loopback_ip: str, port: int):   
     '''
         Receive from the other active script 
         the new data for origin object.
-    '''    
-    loopback_ip = input('\nWrite loopback ip: ')
-    port = int(input('\nWrite Free port: '))
-    
+
+        Parameters:
+            loopback_ip, ip INET address for the other device (use 'ip addr' or 'ifconfig' in bash)
+            port, a free listen port (use 'netstat -ln' in bash)
+    '''
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as load:
         load.bind((loopback_ip, port))
         load.listen()
+        load.accept()
         with load.accept() as connection:
             # Receive seconds and ID from the other script
-            data = connection.recv(512)
-            origin.senconds += data.decode('utf-8')
-            data = connection.recv(512)
+            data = connection.recv(28)
+            origin.senconds += int(data.decode('utf-8'))
+            # After read bytes for seconds read bytes for hash ID
+            data = connection.recv(62)
             origin.ID += data.decode('uft-8')
+            # Send for other script    
+            load.sendto (   [
+                                destination.seconds, 
+                                destination.ID
+                            ],
+                            loopback_ip
+                        )                
 # Show initial state
 def save_wallet(first: TimeWallet, name: str):
     '''
@@ -41,20 +51,28 @@ def save_wallet(first: TimeWallet, name: str):
     with open(name, 'wb') as one:
         one.write(first.seconds.to_bytes())        
 
-def show_wallets(start: TimeWallet, end: TimeWallet):
+def show_wallets(start: TimeWallet, end: TimeWallet) -> str:
     '''
         Print in screen the objects info
     '''
     sep = ''
     for repeat in range(0, 29):
         sep += '-'
-    print(f'\t{sep}\n\t{start.ID} [Your] wallet has {start.seconds}s\n\t[Waiting] wallet has {end.seconds}s')
+    result = f'\t{sep}\n\t{start.ID} [Your] wallet has {start.seconds}s\n\t[Waiting] wallet has {end.seconds}s'
+    del sep
+    print(result)
+    return result
 # First state
 print('\n\t[CRONOMASTER]\n\nSend seconds between conected wallets')
 show_wallets(origin, destination)    
 # Make the right changes
 option = 'y'
 while option.lower() == 'y':
+    # Ask for ip and port for get info
+    try:
+        communication(input('\n\tLoop Back IP from sender: '), int(input('\n\tFree LISTEN port: ')))
+    except:
+        print('\nConnection Fail: Write a valid cronomaster IP and free port')    
     # Send seconds and count more seconds only when has underrate
     origin.send(int(input(f'\n\tWrite seconds to send from {origin.ID}: ')), destination)
     # Next state after send seconds to destination
