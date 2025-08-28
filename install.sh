a9ac75d5411d588b579ad7f0219058ce3cdfff82
Martina Pauer <martinapauer38@gmail.com>
2025-08-28 14:31:11 +0000

    Fix Script for install from source

--- install.sh
+++ install.sh
@@ -1,27 +1,25 @@
 #!/usr/bin/bash
-# Script for install in any distro
-
+# Script for install cronomaster from source in any linux distro
 cronomaster_dir=/usr/cronomaster/
 timeout=3
-graphical="${cronomaster_dir}/src/graphics/cronomaster_gtk.py"
-terminal="${cronomaster_dir}/src/cronomaster.py"
+graphical="${cronomaster_dir}src/graphics/cronomaster_gtk.py"
+graphical_command="/usr/bin/cronomaster_gtk"
+terminal="${cronomaster_dir}src/cronomaster.py"
+terminal_command="/usr/bin/cronomaster"
 
-sudo -k mkdir $cronomaster_dir
-echo "Copy files to ${cronomaster_dir} folder..."
-cp -R ./ $cronomaster_dir
+echo -e "\tINSTALLING [CRONOMASTER]"
+sleep $timeout
+echo -e "\nCopy files to ${cronomaster_dir} folder..."
+sudo -k mkdir -p $cronomaster_dir && cp -R "./" "${cronomaster_dir}"
 sleep $timeout
 echo "Making ${graphical} and ${terminal} executable..."
-chmod 755 $graphical
-chmod 755 $terminal
+sudo -k chmod 755 $graphical && chmod 755 $terminal
 sleep $timeout
-# This part need root privileges using su session
+# Create commands for run in bash terminal
 echo "Generating commands..."
-sudo su
-echo -e "#\!/usr/bin/bash\n${graphical}" >> /usr/bin/cronomaster_gtk
-echo -e "#\!/usr/bin/bash\n${terminal}" >> /usr/bin/cronomaster
-chmod 755 /usr/bin/cronomaster_gtk
-chmod 755 /usr/bin/cronomaster
+sudo -k echo -e "#\!/usr/bin/bash\n${graphical}" >> $graphical_command && echo -e "#\!/usr/bin/bash\n${terminal}" >> $terminal_command
+# Make executuble the commands
+sudo -k chmod 755 $graphical_command && chmod 755 $terminal_command
 # End root privileges for security
-sudo -k
 sleep $timeout
-echo "Now could use cronomaster_gtk and cronomaster commands."
+echo -e "\n\tNow you could use 'cronomaster_gtk' and 'cronomaster' commands.\n"
