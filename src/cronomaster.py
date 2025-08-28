a0cf8d111f06c5bbd7dac75778030ff9da69484f
Martina Pauer <martinapauer38@gmail.com>
2025-08-28 15:13:59 +0000

    Fix directories with new standard paths

--- src/cronomaster.py
+++ src/cronomaster.py
@@ -9,7 +9,7 @@ sys.path.append (
                         os.path.join 
                         (
                             os.path.dirname(__file__),
-                            '../lib'
+                            '/usr/cronomaster/lib'
                         )
                     )
                 )
