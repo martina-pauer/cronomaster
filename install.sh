83241340325c3af88ff5fad9793dfecb584a4f25
Martina Pauer <martinapauer38@gmail.com>
2025-08-29 12:14:41 +0000

    Fix install.sh permission issue

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ mkdir -p $cronomaster_dir && cp -R "./" "${cronomaster_dir}"
 chmod -R 666 $cronomaster_dir
 sleep $timeout
 echo "Making ${graphical} and ${terminal} executable..."
-chmod 755 $graphical && chmod 755 $terminal
+
 sleep $timeout
 # Create commands for run in bash terminal
 echo "Generating commands..."
