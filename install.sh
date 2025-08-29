f37b1e7886ed1f6ad159407d783e8ceb13d3ca20
Martina Pauer <martinapauer38@gmail.com>
2025-08-29 12:19:41 +0000

    Fix perssions and double running on install.sh

--- install.sh
+++ install.sh
@@ -11,15 +11,16 @@ echo -e "\tINSTALLING [CRONOMASTER]"
 sleep $timeout
 echo -e "\nCopy files to ${cronomaster_dir} folder..."
 mkdir -p $cronomaster_dir && cp -R "./" "${cronomaster_dir}"
-chmod -R 666 $cronomaster_dir
+# This is usnafe but need for run the command by the user
+chmod -R 777 $cronomaster_dir
 sleep $timeout
 echo "Making ${graphical} and ${terminal} executable..."
-
 sleep $timeout
 # Create commands for run in bash terminal
 echo "Generating commands..."
+# Rmove if exist old files to commands for avoid double running
+rm -f $graphical_command $terminal_command
 echo -e "#\!/usr/bin/bash\n${graphical}" >> $graphical_command && echo -e "#\!/usr/bin/bash\n${terminal}" >> $terminal_command
 # Make executuble the commands
-chmod 755 $graphical_command && chmod 755 $terminal_command
 sleep $timeout
 echo -e "\n\tNow you could use 'cronomaster_gtk' and 'cronomaster' commands.\n"
