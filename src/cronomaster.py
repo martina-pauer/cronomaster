caa7cf8100de72edb55329c6a7730f6bc0d57183
Martina Pauer <martinapauer38@gmail.com>
2025-09-05 12:12:56 +0000

    Othe fix path

--- src/cronomaster.py
+++ src/cronomaster.py
@@ -11,7 +11,7 @@ sys.path.append (
                         os.path.join 
                         (
                             os.path.dirname(__file__),
-                            f'{prefix}lib'
+                            f'{prefix}lib/'
                         )
                     )
                 )
