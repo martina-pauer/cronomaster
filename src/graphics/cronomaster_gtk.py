4bdc038716f18c6b8a7525135717fd3e944a087d
Martina Pauer <martinapauer38@gmail.com>
2025-09-05 11:35:18 +0000

    Still fixing path

--- src/graphics/cronomaster_gtk.py
+++ src/graphics/cronomaster_gtk.py
@@ -13,7 +13,7 @@ sys.path.append (
                         (
                             os.path.dirname(__file__),
                             # Set path to cronomaster.py file
-                            f'{os.path.abspath('..')}/'
+                            f'{os.path.abspath("../.")}/'
                         )    
                     )
                 )
